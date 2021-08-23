c9fb1339befbcd3bdd5d55469f2b2b4f38ec08d4
MannyMb <emmanuelnefo98@gmail.com>
2021-08-23 13:37:58 -0400

    set up

---
+++ src/main/java/main/controllers/UserController.java
@@ -0,0 +1,12 @@
+package main.controllers;
+
+import main.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+}
---
+++ src/main/java/main/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package main.repositories;
+
+import main.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
