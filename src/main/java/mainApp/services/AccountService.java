dd4f6d0e7aa0bff2019288f4516e91a58b963f94
MannyMb <emmanuelnefo98@gmail.com>
2021-08-25 21:15:41 -0400

    frontend/backend

--- src/main/java/mainApp/services/AccountService.java
+++ src/main/java/mainApp/services/AccountService.java
@@ -50,4 +50,35 @@ public class AccountService {
         return delete(account.getId());
     }
 
+    public Account transfer(Account account1, Account account2, Double amountToTransfer) {
+
+        // Account1 is where we are transfering from -> get the amount in account1 -> substract amountToTransfer
+        // Account2 is where we are sending to -> get the amount in account2 -> add amountToTransfer
+        // return your new balances are + account1.getamount + account2.getaccount
+
+
+//        Double amount1 = account1.getAmount() - amountToTransfer;  // have to make logic reference specific account using the id.
+//        Double amount2 = account2.getAmount() + amountToTransfer;
+//
+//        account1.setAmount(amount1);
+//        account2.setAmount(amount2);
+
+        return account1;
+    }
+
+    public Account deposit(Long id, Double amountToDeposit) { // using id to reference the specific account
+        Account originalAccount = accountRepository.findOne(id);
+        Double amount = originalAccount.getAmount() + amountToDeposit;
+        originalAccount.setAmount(amount);
+
+        return originalAccount;
+    }
+
+    public Account withdraw(Long id, Double amountToWithdraw) { // using id to reference the specific account
+        Account originalAccount = accountRepository.findOne(id);
+        Double amount = originalAccount.getAmount() - amountToWithdraw;
+        originalAccount.setAmount(amount);
+
+        return originalAccount;
+    }
 }
