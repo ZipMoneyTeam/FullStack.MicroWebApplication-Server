763252adb2176eed839997714fe8f4ce58ea12d2
sitarameez <sitarablossom@gmail.com>
2021-08-27 12:26:03 -0400

    so far good

--- moneymanagementapp/src/app/app.module.ts
+++ moneymanagementapp/src/app/app.module.ts
@@ -1,19 +1,33 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
-import { AppUserService } from './appUser.service';
+import { AppUsersComponent } from './appUsers/appUsers.component';
+import { UserLoginsComponent } from './userLogins/userLogins.component';
+import { RouterModule,Routes } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
+
+const routes:Routes=[
+  { path: '',component:UserLoginsComponent},
+  {path:'appUsers',component:AppUsersComponent},
+  {path:'create-account',component:CreateAccountComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppUsersComponent,
+    UserLoginsComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [AppUserService],
+  providers: [AppUsersComponent,UserLoginsComponent],        
   bootstrap: [AppComponent]
 })
 export class AppModule { }
