8d9682c35535dc9cd8ffb925e5e3116ac9883f2c
MannyMb <emmanuelnefo98@gmail.com>
2021-09-03 20:09:55 -0400

    heroku test

---
+++ src/main/java/mainApp/config/AppConfig.java
@@ -0,0 +1,22 @@
+//package mainApp.config;
+//
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+//import javax.validation.constraints.NotNull;
+//
+//
+//@ConfigurationProperties(prefix = "app")
+//public class AppConfig {
+//    @NotNull
+//    private String url;
+//
+//
+//    public String getUrl() {
+//        return url;
+//    }
+//
+//    public void setUrl(String url) {
+//        this.url = url;
+//    }
+//}
